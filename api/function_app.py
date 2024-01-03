16dff6ab39b78c3b4254e9cc042037a02ff5774d
Enrique Acuay <e.acuay@gmail.com>
2024-01-03 09:24:20 +0100

    adding blueprint example

--- api/function_app.py
+++ api/function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
+from blueprint import blueprint as blabla
 import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app.register_blueprint(blabla)
 
 @app.route(route="http_trigger_stream")
 def http_trigger_stream(req: func.HttpRequest) -> func.HttpResponse:
@@ -23,3 +25,25 @@ def http_trigger_stream(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="http_trigger_negotiate", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger_negotiate(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
